508bf89314910aa86570463208adf6d15ba37def
safae <bensaid.safae@gmail.com>
2020-01-12 18:10:12 +0100

    ajout des attributs plus getter et setter

--- src/fr/dawan/formation/model/Salle.java
+++ src/fr/dawan/formation/model/Salle.java
@@ -2,11 +2,22 @@
  * 
  */
 package fr.dawan.formation.model;
+import java.util.Date;
 
 /**
  * @author Admin-Stagiaire
  *
  */
+
 public class Salle {
 
+	private long codeSalle;
+	private String nom;
+	private int capaciteAssise;
+	private int capaciteDebout;
+	private String numeroTelephone;
+	private boolean occupation;
+	private Date heureOuverture;
+	
+	 
 }
